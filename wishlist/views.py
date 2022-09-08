eab53f6688b336b693cce1abae7c001f623d0a7c
Laura Glory <laura.toruan@gmail.com>
2022-09-08 16:31:19 +0700

    @wishlist

---
+++ wishlist/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+from django.shortcuts import render
+
+# Create your views here.
+def show_wishlist(request):
+    
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Laura'
+    }
+    return render(request, "wishlist.html", context)
